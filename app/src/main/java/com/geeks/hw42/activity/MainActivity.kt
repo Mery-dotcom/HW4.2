a98075864778d003200a4a2ce2a61b1c29f2beda
Mery-dotcom <meka-28list.ru>
2025-02-07 22:14:45 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/hw42/activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.geeks.hw42.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.geeks.hw42.R
+import com.geeks.hw42.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
